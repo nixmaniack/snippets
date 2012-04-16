72866c48e57b08b874393de7481aff8d77be30fc
Muneeb Shaikh <iammuneeb@gmail.com>
2012-04-16 19:25:07 +0530

    Beautified output a bit

--- random/clifu-CoD.py
+++ random/clifu-CoD.py
@@ -5,5 +5,9 @@ import json
 
 jdata = json.loads(urllib2.urlopen('http://www.commandlinefu.com/commands/random/json').read())
 
+print
 print "Summary: " + jdata[0]['summary']
-print "Command: " + jdata[0]['command']
+print
+print "Command: $ " + jdata[0]['command']
+print
+print "Details: " + jdata[0]['url']
